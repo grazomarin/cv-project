88b617525a92bc6269661bca3cb73b527cc486cf
grazomarin <huseynovk355@gmail.com>
2023-01-31 19:56:11 +0400

    added Skills edit and add functions

--- src/components/right_half/Skills.jsx
+++ src/components/right_half/Skills.jsx
@@ -3,35 +3,127 @@ import '../../styles/Skills.scss';
 import uniqid from 'uniqid';
 import EditButton from '../EditButton';
 import AddButton from '../AddButton';
+import EditForm from '../EditForm';
 
 class SkillItem extends Component {
+	render() {
+		return <li>{this.props.item.skill || '<empty>'}</li>;
+	}
+}
+
+class SkillItemEdit extends Component {
 	state = {
-		name: this.props.item.name,
+		skill: this.props.item.skill,
 		id: this.props.item.id,
 	};
 
 	render() {
-		return <li>{this.state.name}</li>;
+		return (
+			<li>
+				<input
+					type="text"
+					name="skill"
+					value={this.state.skill}
+					onChange={(e) => {
+						this.setState({
+							...this.state,
+							skill: e.target.value,
+						});
+					}}
+				/>
+			</li>
+		);
 	}
 }
 
 export default class Skills extends Component {
 	state = {
-		items: [{ name: '--skill--', id: uniqid() }],
+		items: [{ skill: 'skill', id: uniqid() }],
+		active: false,
 	};
-	render() {
+
+	toggleEdit = (e) => {
+		e?.preventDefault();
+		this.setState({
+			...this.state,
+			active: !this.state.active,
+		});
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		let updated;
+		if (e.target.skill.length >= 2) {
+			updated = Array.from(e.target.skill).map((skill) => {
+				return {
+					skill: skill.value,
+					id: uniqid(),
+				};
+			});
+		} else {
+			updated = [
+				{
+					skill: e.target.skill.value,
+					id: uniqid(),
+				},
+			];
+		}
+		this.setState({
+			items: updated,
+			active: false,
+		});
+	};
+
+	createItem = () => {
+		this.setState({
+			items: [
+				...this.state.items,
+				{
+					skill: '',
+					id: uniqid(),
+				},
+			],
+		});
+	};
+
+	renderEdit = () => {
 		return (
-			<div className="skills">
-				<div className="category">Skills</div>
-				<ul>
+			<ul className="list--edit">
+				<EditForm
+					submit={(e) => this.handleSubmit(e)}
+					cancel={(e) => this.toggleEdit(e)}
+				>
+					{this.state.items.map((item) => {
+						return <SkillItemEdit item={item} key={item.id} />;
+					})}
+				</EditForm>
+				<div className="actionCont">
+					<AddButton func={{ createItem: this.createItem }} />
+				</div>
+			</ul>
+		);
+	};
+
+	renderDisplay = () => {
+		return (
+			<>
+				<ul className="list">
 					{this.state.items.map((item) => {
 						return <SkillItem item={item} key={item.id} />;
 					})}
 				</ul>
 				<div className="actionCont">
-					<AddButton />
-					<EditButton active={this.state.active} />
+					<EditButton toggleEdit={this.toggleEdit} />
 				</div>
+			</>
+		);
+	};
+
+	render() {
+		return (
+			<div className="skills">
+				<div className="category">Skills</div>
+				{this.state.active ? this.renderEdit() : this.renderDisplay()}
 			</div>
 		);
 	}
