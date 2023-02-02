b109566890aee03409aa5db06cf7f33ad4023591
grazomarin <huseynovk355@gmail.com>
2023-02-02 15:13:10 +0400

    feat: add ability to change profile picture

--- src/components/right_half/Avatar.jsx
+++ src/components/right_half/Avatar.jsx
@@ -3,7 +3,37 @@ import placeholder from '../../images/placeholder.jpg';
 import '../../styles/Avatar.scss';
 
 export default class Avatar extends Component {
+	state = {
+		src: placeholder,
+	};
+
+	validateFileExtension = (file) => {
+		const allowedExtensions = /(jpg|jpeg|svg|png)/;
+		if (allowedExtensions.exec(file.type)) return true;
+	};
+
+	handleImageChange = (e) => {
+		const newImage = e.target.files[0];
+		if (this.validateFileExtension(newImage)) {
+			this.setState({ src: URL.createObjectURL(newImage) });
+		}
+	};
+
 	render() {
-		return <img src={placeholder} alt="" className="avatar" />;
+		return (
+			<div className="avatar">
+				<img src={this.state.src} alt="" className="avatar_img" />
+				<label htmlFor="img-upload" className="avatar_input">
+					Upload Image
+				</label>
+				<input
+					id="img-upload"
+					type="file"
+					accept=".jpg,.jpeg,.svg,.png"
+					name="file1"
+					onChange={this.handleImageChange}
+				/>
+			</div>
+		);
 	}
 }
