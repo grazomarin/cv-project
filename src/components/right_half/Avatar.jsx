220b16bb58ba1fb7c2cd6b8effa8d52b41594d8c
grazomarin <huseynovk355@gmail.com>
2023-02-02 16:00:26 +0400

    feat(component): display error message on invalid pfp format upload
    added commitizen to dependencies

--- src/components/right_half/Avatar.jsx
+++ src/components/right_half/Avatar.jsx
@@ -5,11 +5,25 @@ import '../../styles/Avatar.scss';
 export default class Avatar extends Component {
 	state = {
 		src: placeholder,
+		error: false,
+	};
+
+	toggleError = () => {
+		this.setState({ error: !this.state.error });
+	};
+
+	renderError = () => {
+		return <div className="error">Error: Invalid format</div>;
 	};
 
 	validateFileExtension = (file) => {
 		const allowedExtensions = /(jpg|jpeg|svg|png)/;
-		if (allowedExtensions.exec(file.type)) return true;
+		if (allowedExtensions.exec(file.type)) {
+			return true;
+		} else {
+			this.toggleError();
+			setTimeout(this.toggleError, 3000);
+		}
 	};
 
 	handleImageChange = (e) => {
@@ -22,6 +36,7 @@ export default class Avatar extends Component {
 	render() {
 		return (
 			<div className="avatar">
+				{this.state.error ? this.renderError() : null}
 				<img src={this.state.src} alt="" className="avatar_img" />
 				<label htmlFor="img-upload" className="avatar_input">
 					Upload Image
