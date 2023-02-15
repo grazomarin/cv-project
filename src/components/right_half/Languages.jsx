8c333312d8cddf2aff030b0995bc99ab5c790770
grazomarin <huseynovk355@gmail.com>
2023-02-15 21:58:43 +0400

    refactor(component): switch from Languages class to factory

--- src/components/right_half/Languages.jsx
+++ src/components/right_half/Languages.jsx
@@ -1,214 +1,174 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../../styles/Languages.scss';
 import uniqid from 'uniqid';
 import EditButton from '../EditButton';
 import AddButton from '../AddButton';
 import EditForm from '../EditForm';
-import DeleteButton from '../DeleteButton';
+import RemoveButton from '../RemoveButton';
 
-class LanguageItem extends Component {
-	state = {
-		language: this.props.item.language,
-		level: this.props.item.level,
+const Languages = () => {
+	const [languages, setLanguages] = useState([
+		{ id: uniqid(), language: '', level: '1' },
+	]);
+	const [active, setActive] = useState(false);
+
+	const toggleEdit = () => setActive(!active);
+
+	const handleAdd = () => {
+		setLanguages((prev) => [
+			...prev,
+			{ id: uniqid(), language: '', level: '1' },
+		]);
 	};
 
-	render() {
-		return (
-			<div className="language">
-				<div className="language_title">
-					<div>
-						<span>{this.state.language || '<empty>'}</span>{' '}
-						<DeleteButton
-							delete={() => {
-								this.props.func.delete(this.props.item.id);
-							}}
-						/>
-					</div>
-					<span>{this.props.func.returnLevel(this.state.level)}</span>
-				</div>
-				<input
-					type="range"
-					min="1"
-					max="6"
-					value={this.state.level}
-					onChange={(e) => this.setState({ level: e.target.value })}
-				/>
-			</div>
+	const handleRemove = (id) => {
+		setLanguages((prev) =>
+			prev.filter((language) => !(language.id === id))
 		);
-	}
-}
-
-class LanguageItemEdit extends Component {
-	state = {
-		language: this.props.item.language,
-		level: this.props.item.level,
 	};
 
-	render() {
-		return (
-			<div className="language">
-				<div className="language_title--edit">
-					<input
-						type="text"
-						name="language"
-						value={this.state.language}
-						onChange={(e) =>
-							this.setState({ language: e.target.value })
-						}
-					/>
-					<span>{this.props.func.returnLevel(this.state.level)}</span>
-				</div>
-				<input
-					type="range"
-					name="level"
-					min="1"
-					max="6"
-					value={this.state.level}
-					onChange={(e) => this.setState({ level: e.target.value })}
-				/>
-			</div>
-		);
-	}
-}
-
-export default class Languages extends Component {
-	state = {
-		items: [
-			{
-				language: 'English',
-				level: '5',
-				id: uniqid(),
-			},
-		],
-		active: false,
-	};
-
-	returnLevel(lev) {
-		return lev === '1'
-			? 'A1'
-			: lev === '2'
-			? 'A2'
-			: lev === '3'
-			? 'B1'
-			: lev === '4'
-			? 'B2'
-			: lev === '5'
-			? 'C1'
-			: lev === '6'
-			? 'C2'
-			: '<none>';
-	}
-
-	toggleEdit = (e) => {
-		e?.preventDefault();
-		this.setState({
-			...this.state,
-			active: !this.state.active,
-		});
-	};
-
-	handleSubmit = (e) => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
-		let updated = [];
-		if (e.target.language?.length >= 2) {
-			for (let i = 0; i < e.target.language.length; i++) {
-				updated.push({
-					language: e.target.language[i].value,
-					level: e.target.level[i].value,
-					id: uniqid(),
-				});
-			}
-		} else {
-			updated = [
-				{
-					language: e.target.language?.value,
-					level: e.target.level?.value,
-					id: uniqid(),
-				},
-			];
+		if (!e.target.language) return;
+		const updated = Array.from(e.target.language);
+
+		switch (updated.length) {
+			case 0:
+				setLanguages([
+					{
+						id: uniqid(),
+						language: e.target.language.value,
+						level: e.target.level.value,
+					},
+				]);
+				break;
+
+			default:
+				setLanguages(
+					(function rec(i = 0) {
+						if (i === updated.length) return [];
+						return [
+							{
+								id: uniqid(),
+								language: e.target.language[i].value,
+								level: e.target.level[i].value,
+							},
+							...rec((i = i + 1)),
+						];
+					})()
+				);
+				break;
 		}
-		this.setState({
-			items: updated,
-			active: false,
-		});
+		toggleEdit();
 	};
 
-	handleDelete = (id) => {
-		this.state.items.forEach((item) => {
-			if (item.id === id) {
-				this.state.items.splice(this.state.items.indexOf(item), 1);
-			}
-		});
-		this.setState({ items: this.state.items });
-	};
+	const handleCancel = () => toggleEdit();
 
-	createItem = () => {
-		this.setState({
-			items: [
-				...this.state.items,
-				{
-					language: '',
-					level: '1',
-					id: uniqid(),
-				},
-			],
-		});
-	};
-
-	renderEdit = () => {
-		return (
-			<div className="languages-cont">
-				<EditForm
-					submit={(e) => this.handleSubmit(e)}
-					cancel={(e) => this.toggleEdit(e)}
-				>
-					{this.state.items.map((item) => {
-						return (
-							<LanguageItemEdit
-								item={item}
-								key={item.id}
-								func={{
-									returnLevel: this.returnLevel,
-								}}
-							/>
-						);
-					})}
-				</EditForm>
-				<div className="actionCont">
-					<AddButton func={{ createItem: this.createItem }} />
-				</div>
+	const renderDisplay = () => (
+		<div className="languages-cont">
+			{languages.map((language) => {
+				return (
+					<LanguageItem
+						key={language.id}
+						id={language.id}
+						active={active}
+						language={language.language}
+						level={language.level}
+						remove={handleRemove}
+					/>
+				);
+			})}
+			<div className="actionCont">
+				<EditButton toggleEdit={toggleEdit} />
 			</div>
-		);
-	};
+		</div>
+	);
 
-	renderDisplay = () => {
-		return (
-			<div className="languages-cont">
-				{this.state.items.map((item) => {
+	const renderEdit = () => (
+		<div className="languages-cont">
+			<EditForm submit={handleSubmit} cancel={handleCancel}>
+				{languages.map((language) => {
 					return (
 						<LanguageItem
-							item={item}
-							key={item.id}
-							func={{
-								returnLevel: this.returnLevel,
-								delete: this.handleDelete,
-							}}
+							key={language.id}
+							id={language.id}
+							active={active}
+							language={language.language}
+							level={language.level}
+							remove={handleRemove}
 						/>
 					);
 				})}
-				<div className="actionCont">
-					<EditButton toggleEdit={this.toggleEdit} />
-				</div>
+			</EditForm>
+			<div className="actionCont">
+				<AddButton add={handleAdd} />
 			</div>
-		);
+		</div>
+	);
+
+	return (
+		<div className="languages">
+			<div className="category">Languages</div>
+			{active ? renderEdit() : renderDisplay()}
+		</div>
+	);
+};
+
+const LanguageItem = ({ id, active, remove, ...props }) => {
+	const [language, setLanguage] = useState(props.language);
+	const [level, setLevel] = useState(props.level);
+
+	const returnLevel = (lvl) => {
+		return lvl === '1'
+			? 'A1'
+			: lvl === '2'
+			? 'A2'
+			: lvl === '3'
+			? 'B1'
+			: lvl === '4'
+			? 'B2'
+			: lvl === '5'
+			? 'C1'
+			: lvl === '6'
+			? 'C2'
+			: '<none>';
 	};
 
-	render() {
-		return (
-			<div className="languages">
-				<div className="category">Languages</div>
-				{this.state.active ? this.renderEdit() : this.renderDisplay()}
+	const renderDisplay = () => (
+		<div className="language_title">
+			<div>
+				<span>{language || '<empty>'}</span>{' '}
+				<RemoveButton remove={() => remove(id)} />
 			</div>
-		);
-	}
-}
+			<span>{returnLevel(level)}</span>
+		</div>
+	);
+
+	const renderEdit = () => (
+		<div className="language_title--edit">
+			<input
+				type="text"
+				name="language"
+				value={language}
+				onChange={(e) => setLanguage(e.target.value)}
+			/>
+			<span>{returnLevel(level)}</span>
+		</div>
+	);
+
+	return (
+		<div className="language">
+			{active ? renderEdit() : renderDisplay()}
+			<input
+				type="range"
+				name="level"
+				min="1"
+				max="6"
+				value={level}
+				onChange={(e) => setLevel(e.target.value)}
+			/>
+		</div>
+	);
+};
+
+export default Languages;
