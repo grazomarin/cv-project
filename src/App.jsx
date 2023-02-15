966e2e2cb0b5be99048be0c5d5da4a8f32088d23
grazomarin <huseynovk355@gmail.com>
2023-02-15 22:01:54 +0400

    refactor: switch from App and CV classes to factories

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import logo from './images/logo.svg';
 import ReactToPrint from 'react-to-print';
 import CV from './components/CV';
 import './styles/App.scss';
 
-class App extends Component {
-	render() {
-		return (
-			/* throws warning because react-to-print uses findDOMNode */
-			<>
-				<div className="header">
-					<img className="logo" src={logo} alt="" />
-					<ReactToPrint
-						trigger={() => {
-							return (
-								<button
-									className="download"
-									onClick={this.printPdf}
-								>
-									Download as PDF
-								</button>
-							);
-						}}
-						content={() => this.componentRef}
-						documentTitle="myCV"
-					/>
-				</div>
-				<CV ref={(el) => (this.componentRef = el)} />
-			</>
-		);
-	}
-}
+const App = () => {
+	const componentRef = useRef();
+
+	return (
+		<>
+			<div className="header">
+				<img className="logo" src={logo} alt="" />
+				<ReactToPrint
+					trigger={() => (
+						<button className="download">Download as PDF</button>
+					)}
+					content={() => componentRef.current}
+					documentTitle="myCV"
+				/>
+			</div>
+			<CV ref={componentRef} />
+		</>
+	);
+};
 
 export default App;
--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Title from './left_half/Title';
 import Experience from './left_half/Experience';
 import Education from './left_half/Education';
@@ -8,25 +8,23 @@ import Details from './right_half/Details';
 import Skills from './right_half/Skills';
 import Languages from './right_half/Languages';
 
-class CV extends Component {
-	render() {
-		return (
-			<div className="cv">
-				<div className="left">
-					<Title />
-					<About />
-					<Education />
-					<Experience />
-				</div>
-				<div className="right">
-					<Avatar />
-					<Details />
-					<Skills />
-					<Languages />
-				</div>
+const CV = React.forwardRef((props, ref) => {
+	return (
+		<div className="cv" ref={ref}>
+			<div className="left">
+				<Title />
+				<About />
+				<Education />
+				<Experience />
 			</div>
-		);
-	}
-}
+			<div className="right">
+				<Avatar />
+				<Details />
+				<Skills />
+				<Languages />
+			</div>
+		</div>
+	);
+});
 
 export default CV;
